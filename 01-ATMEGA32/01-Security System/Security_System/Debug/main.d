463f28cda953071a16dc5195a0d4768d6a910c15
osama <gad153879@gmail.com>
2021-09-27 12:05:54 +0200

    GPIO/SEV_SEG

---
+++ 01-ATMEGA32/01-Security System/Security_System/Debug/main.d
@@ -0,0 +1,45 @@
+main.d main.o: .././main.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../LIB/Mem_Map32.h ../LIB/STD_TYPES.h ../LIB/BIT_MATH.h \
+ ../MCAL/DIO_interface.h ../MCAL/DIO_config.h ../HAL/Switch.h \
+ ../HAL/LED.h ../HAL/SEG_interface.h ../HAL/SEG_config.h \
+ ../HAL/SEG_private.h .././APP.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../LIB/Mem_Map32.h:
+
+../LIB/STD_TYPES.h:
+
+../LIB/BIT_MATH.h:
+
+../MCAL/DIO_interface.h:
+
+../MCAL/DIO_config.h:
+
+../HAL/Switch.h:
+
+../HAL/LED.h:
+
+../HAL/SEG_interface.h:
+
+../HAL/SEG_config.h:
+
+../HAL/SEG_private.h:
+
+.././APP.h:
